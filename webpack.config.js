c7bea4aa946fe596b1dbe20c4778afcf855fdac5
hanson.wang <hanson.wng@gmail.com>
2015-09-30 01:56:08 -0700

    babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.js$/ , loader: 'babel-loader', query: {stage: 1, optional: 'runtime'} },
+      { test: /\.js$/ , loader: 'babel-loader' },
       // font-awesome
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
